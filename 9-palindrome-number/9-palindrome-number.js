3ce2dd4e65ffb6114dadb79a79f524105d350284
Sarah Mohamed <44445357+SarahMohamedAbdAlkader@users.noreply.github.com>
2022-05-23 23:48:31 +0200

    Time: 368 ms (12.47%), Space: 50.2 MB (91.38%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -2,8 +2,46 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    x= String(x)
+var isPalindromeI = function(x) {
+x= String(x)
 const reversedArray=x.split('').reverse().join('');
 return x === reversedArray;
 };
+
+var isPalindromeII = function(x) {
+const xAsArray = convertNumberToArray(x)
+const xAsArrayReversed = xAsArray.reverse()
+
+for(let i = 0; i < xAsArray.length; i++) {
+    if (xAsArray[i] != xAsArrayReversed[i]) {
+        return false
+    }
+}
+return true
+};
+
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false
+    }
+const xAsArray = convertNumberToArray(x)
+const midIndex = Math.floor(xAsArray.length / 2)
+
+for(let i = 0; i < midIndex; i++) {
+    if (xAsArray[i] != xAsArray[xAsArray.length - 1 - i]) {
+        return false
+    }
+}
+return true
+};
+
+var convertNumberToArray = function(x) {
+    const result = []
+    
+    while (x != 0) {
+        result.push(x % 10)
+        x = Math.floor(x / 10)
+    }
+    
+    return result
+}
