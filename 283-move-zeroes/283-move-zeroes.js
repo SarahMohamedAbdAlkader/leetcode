e8187b5781ed34afb0f43ca9feee12370aad9440
Sarah Mohamed <44445357+SarahMohamedAbdAlkader@users.noreply.github.com>
2022-06-07 21:12:00 +0200

    Time: 159 ms (22.85%), Space: 46.2 MB (94.70%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    const length = nums.length;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    for (let i = 0; i < length; i++) {
+        if (!nums[i]) {
+            nums[i] = 0;
+
+        }
+    }
+    return nums
+};
