056d5de05a0468bcfd3cd7058923487d54511347
Sarah Mohamed <44445357+SarahMohamedAbdAlkader@users.noreply.github.com>
2024-09-16 14:54:30 +0300

    Time: 65 ms (13.39%) | Memory: 49.1 MB (25.33%) - LeetSync

---
+++ 2762-cache-with-time-limit/cache-with-time-limit.js
@@ -0,0 +1,49 @@
+var TimeLimitedCache = function () {
+    this.cachedObject = new Map()
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+
+
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+    const existed = this.cachedObject.has(key);
+
+    if (existed) {
+        clearTimeout(this.cachedObject.get(key).timer)
+    }
+    const timeout = setTimeout(() => {
+        this.cachedObject.delete(key);
+    }, duration);
+
+    this.cachedObject.set(key, { value, timer: timeout })
+    return existed
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function (key) {
+    console.log(this.cachedObject)
+    const existedValue = this.cachedObject.has(key)
+    return existedValue ? this.cachedObject.get(key).value : -1
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function () {
+    return this.cachedObject.size
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
